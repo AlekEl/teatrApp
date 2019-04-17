1bd0f27d156ee372a2210780d4cd53c1284bcf48
danielmielczarek <daniel1112m@gmail.com>
2019-04-17 12:03:06 +0200

    Merge with ActorsDao

--- src/main/java/com/codecool/teatr/service/ActorService.java
+++ src/main/java/com/codecool/teatr/service/ActorService.java
@@ -1,6 +1,7 @@
 package com.codecool.teatr.service;
 
 import com.codecool.teatr.dao.ActorDAO;
+import com.codecool.teatr.model.Actor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,5 +11,9 @@ public class ActorService {
     @Autowired
     ActorDAO actorDAO;
 
+
+    public void addActor(Actor actor){
+        actorDAO.save(actor);
+    }
 
 }
