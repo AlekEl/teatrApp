91ed6b6cfc73628d233abc2cae5cad3fbe8b39f7
AlekEl <leszczynski.al@gmail.com>
2019-04-17 11:15:54 +0200

    Initial commit

---
+++ src/main/java/com/codecool/teatr/TeatrApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.teatr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeatrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeatrApplication.class, args);
+    }
+
+}
