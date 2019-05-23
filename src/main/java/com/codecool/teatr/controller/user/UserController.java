e7da4b533b4271f678e78e8fc373ae29b8dc0c54
danielmielczarek <daniel1112m@gmail.com>
2019-05-23 09:57:54 +0200

    merge with UserModel branch

--- src/main/java/com/codecool/teatr/controller/user/UserController.java
+++ src/main/java/com/codecool/teatr/controller/user/UserController.java
@@ -12,18 +12,29 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @GetMapping("/h")
-    public String index2() {
-        Address address = new Address("ulica","65","9","00-000","waw");
-        Administrator administrator = new Administrator();
-        administrator.setAddress(address);
-        userService.addUser(administrator);
-        System.out.println(administrator);
-        return "mainPage";
-    }
+//    @GetMapping("/h")
+//    public String index2() {
+//        Address address = new Address("ulica","65","9","00-000","waw");
+//        Administrator administrator = new Administrator();
+//        administrator.setAddress(address);
+//        userService.addUser(administrator);
+//        System.out.println(administrator);
+//        return "mainPage";
+//    }
+//
 
     @GetMapping("/")
-    public String index() {
-        return "redirect:/h";
+    public String mainPage() {
+        return "index";
+    }
+
+    @GetMapping("/register")
+    public String register(){
+        return "registerForm";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "loginPage";
     }
 }
