90f509927becdd9e185ef5fc4bebce2f573799d7
AlekEl <leszczynski.al@gmail.com>
2019-06-03 10:15:29 +0200

    Add constructor to TempUser

--- src/main/java/com/codecool/teatr/model/user/TempUser.java
+++ src/main/java/com/codecool/teatr/model/user/TempUser.java
@@ -1,4 +1,9 @@
 package com.codecool.teatr.model.user;
 
+import com.codecool.teatr.model.contact.Address;
+
 public class TempUser extends User {
+    public TempUser(String name, String surname, String mail, String phone, Address address) {
+        super(name, surname, mail, phone, address);
+    }
 }
